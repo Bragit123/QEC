569e59d47133947cd80b9c2839a99e7999f6d3e2
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2025-06-23 09:25:46 +0200

    Minor update

--- Software_testing/qsurface_test.py
+++ Software_testing/qsurface_test.py
@@ -6,43 +6,43 @@ mpl.use("Qt5Agg")
 my_params = PlotParams(scale_figure_length=7, scale_figure_height=7)
 
 
-# code, decoder = qsm.initialize(
-#     size=(5, 5), # Lattice size
-#     Code="toric", # Surface topology
-#     Decoder="mwpm", # Algorithm for decoding. "mwpm" = Minimal Weight Perfect Matching
-#     enabled_errors=["pauli"],
-#     # initial_states=(0,0),
-#     # plotting=True,
-#     # plot_params=my_params,
-#     ### NB: Very prone to mistakes! No error message when including non-existent arguments
-#     # dette_finnes_ikke_men_gir_ikke_feilmelding=0.5
-# )
+code, decoder = qsm.initialize(
+    size=(5, 5), # Lattice size
+    Code="toric", # Surface topology
+    Decoder="mwpm", # Algorithm for decoding. "mwpm" = Minimal Weight Perfect Matching
+    enabled_errors=["pauli"],
+    initial_states=(1,1),
+    plotting=True,
+    plot_params=my_params,
+    ### NB: Very prone to mistakes! No error message when including non-existent arguments
+    dette_finnes_ikke_men_gir_ikke_feilmelding=0.5
+)
 
-# benchmarker = qsm.BenchmarkDecoder({"decode": "duration"}, decoder=decoder)
+# benchmarker = qsm.BenchmarkDecoder({"decode": "duration"})
 # code.random_errors(p_bitflip=0.1)
 # decoder.decode()
 # benchmarker.lists
 
-code, decoder = qsm.initialize((6,6), "toric", "mwpm", enabled_errors=["pauli"])
-benchmarker = qsm.BenchmarkDecoder(
-    {"decode": "duration"},
-    decoder=decoder
-)
-code.random_errors(p_bitflip=0.1)
-decoder.decode()
-benchmarker.lists
-
-# qsm.run(
-#     code=code,
-#     decoder=decoder,
-#     iterations=5, # Number of times to add and decode errors
-#     error_rates={
-#         "p_bitflip": 0.1,
-#         "p_phaseflip": 0.1
-#     }, # Probability of errors occuring
-#     decode_initial=False,
-#     benchmark=benchmarker,
-#     seed=100,
-#     ### NB: Very prone to mistakes! No error message when including non-existent arguments
-#     dette_finnes_ikke_men_gir_ikke_feilmelding=0.5
+# code, decoder = qsm.initialize((6,6), "toric", "mwpm", enabled_errors=["pauli"])
+# benchmarker = qsm.BenchmarkDecoder(
+#     {"decode": "duration"},
+#     decoder=decoder
 # )
+# code.random_errors(p_bitflip=0.1)
+# decoder.decode()
+# benchmarker.lists
+
+qsm.run(
+    code=code,
+    decoder=decoder,
+    iterations=1, # Number of times to add and decode errors
+    error_rates={
+        "p_bitflip": 0.1,
+        "p_phaseflip": 0.1
+    }, # Probability of errors occuring
+    decode_initial=False,
+    # benchmark=benchmarker,
+    seed=100,
+    ### NB: Very prone to mistakes! No error message when including non-existent arguments
+    dette_finnes_ikke_men_gir_ikke_feilmelding=0.5
+)
